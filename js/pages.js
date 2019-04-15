a0aff8679755f4c636daa06154cea9cca8471976
SarahCrossland <SarahCrossland@users.noreply.github.com>
2019-04-15 22:39:08 +0100

    happysong-generator-javascript-slider

--- js/pages.js
+++ js/pages.js
@@ -22,7 +22,7 @@ const pages = [{
     background: "linear-gradient(0deg, rgba(189, 255, 181, 1) 0%, rgba(252, 210, 134, 1) 100%)"
   },
   {
-    content: "loves pretty floral teacups",
+    content: "Build Me Up Buttercup by The Foundations!",
     circle: "#8b4000",
     background: "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)"
   },
